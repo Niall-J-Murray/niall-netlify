87ceadf125fde04bd8a1c39f12cf03134c0a1b24
Niall Murray <98766079+Niall-J-Murray@users.noreply.github.com>
2023-05-03 19:47:30 +0100

    Update index.tsx

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -63,46 +63,16 @@ export default function Index() {
 
         <div className="mx-auto max-w-7xl py-2 px-4 sm:px-6 lg:px-8">
           <div className="mt-6 flex flex-wrap justify-center gap-8">
-<!--             {[
+          {[
               {
-                src: "https://user-images.githubusercontent.com/43764894/223575102-b784690b-19a6-4e7a-b72c-8ac42c736a71.png",
-                alt: "Netlify",
-                href: "https://netlify.com",
+                src: "https://cgfjpzrnyiqtomxrgprp.supabase.co/storage/v1/object/public/slipstream-images/images/background.jpg?t=2023-05-03T18%3A16%3A21.748Z"
+                alt: "Slipstream",
+                href: "https://slipstreamf1-production.up.railway.app/home",
               },
               {
-                src: "https://user-images.githubusercontent.com/8431042/158711352-746c52cf-433e-4823-987a-c9d6f4349ce7.svg",
-                alt: "Supabase",
-                href: "https://supabase.com",
-              },
-              {
-                src: "https://user-images.githubusercontent.com/1500684/157764276-a516a239-e377-4a20-b44a-0ac7b65c8c14.svg",
-                alt: "Tailwind",
-                href: "https://tailwindcss.com",
-              },
-              {
-                src: "https://user-images.githubusercontent.com/1500684/157764454-48ac8c71-a2a9-4b5e-b19c-edef8b8953d6.svg",
-                alt: "Cypress",
-                href: "https://www.cypress.io",
-              },
-              {
-                src: "https://user-images.githubusercontent.com/1500684/157772662-92b0dd3a-453f-4d18-b8be-9fa6efde52cf.png",
-                alt: "Testing Library",
-                href: "https://testing-library.com",
-              },
-              {
-                src: "https://user-images.githubusercontent.com/1500684/157772934-ce0a943d-e9d0-40f8-97f3-f464c0811643.svg",
-                alt: "Prettier",
-                href: "https://prettier.io",
-              },
-              {
-                src: "https://user-images.githubusercontent.com/1500684/157772990-3968ff7c-b551-4c55-a25c-046a32709a8e.svg",
-                alt: "ESLint",
-                href: "https://eslint.org",
-              },
-              {
-                src: "https://user-images.githubusercontent.com/1500684/157773063-20a0ed64-b9f8-4e0b-9d1e-0b65a3d4a6db.svg",
-                alt: "TypeScript",
-                href: "https://typescriptlang.org",
+                src: "https://cgfjpzrnyiqtomxrgprp.supabase.co/storage/v1/object/public/slipstream-images/images/In-Blue-96.png?t=2023-05-03T18%3A46%3A41.175Z",
+                alt: "LinkedIn",
+                href: "https://www.linkedin.com/in/niall-j-murray/",
               },
             ].map((img) => (
               <a
@@ -112,7 +82,7 @@ export default function Index() {
               >
                 <img alt={img.alt} src={img.src} />
               </a>
-            ))} -->
+            ))}
           </div>
         </div>
       </div>
